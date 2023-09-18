654c91148f90dd1afd29ff481dec6fb0619fae97
Nakorvalole <cwotka@yandex.ru>
2023-09-18 21:40:23 +0300

    logic order status

--- src/components/GoodsList.jsx
+++ src/components/GoodsList.jsx
@@ -2,7 +2,10 @@ import GoodsItem from "./GoodsItem";
 import PropsTypes from 'prop-types'
 
 function GoodsList(props) {
-    const {goods = []} = props;
+    const {
+        goods = [],
+        addToBasket,
+    } = props;
 
     if (!goods.length) {
         return <h3>Nothing here</h3>
@@ -10,7 +13,7 @@ function GoodsList(props) {
     return (
         <div className="goods">
             {goods.map(item => (
-                <GoodsItem key = {item.mainId} {...item}/>
+                <GoodsItem key = {item.mainId} {...item} addToBasket={addToBasket}/>
             ))}
         </div>
     )
@@ -20,6 +23,8 @@ function GoodsList(props) {
 
 GoodsList.propTypes = {
     goods: PropsTypes.array,
+    addToBasket: PropsTypes.func,
+
 }
 
 export default GoodsList
